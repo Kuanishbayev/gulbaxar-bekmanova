34fef2296209ed8900d2b7151bdd8477503b19e7
Ernazar <kuanishbayevyernazar@gmail.com>
2024-06-29 18:30:54 +0500

    Docs section style has been changed.

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -6,7 +6,7 @@ const Docs = () => {
                 <h1 className="text-2xl font-bold mb-4">Virtual Kimyo Laboratoriyasi</h1>
                 <p className="mb-2 indent-8">"Virtual Kimyo Laboratoriyasi" ilovasi Windows platformasi uchun C# va WinForms texnologiyalaridan foydalangan holda ishlab chiqilgan innovatsion dasturiy ta'minotdir. Ushbu ilova foydalanuvchilarga xavfsiz va interaktiv muhitda kimyoviy tajribalarni amalga oshirish imkoniyatini yaratadi. Dastur kimyo fanini o‘rganayotgan talabalar, o‘qituvchilar va kimyoga qiziqadigan barcha uchun mo‘ljallangan.</p>
                 <p className="mb-2 indent-8">"Virtual Kimyo Laboratoriyasi" ilovasi foydalanuvchilarga kimyoviy tajribalarni o‘tkazish uchun qulay va xavfsiz vositani taqdim etadi. Bu ilova nafaqat nazariyani o'rganish, balki kimyoviy reaktsiyalarning o'tishini amalda ko'rish imkonini beradi. Zamonaviy texnologiyalar va puxta o'ylangan arxitekturadan foydalanish tufayli, ilova ta'lim va ilmiy tadqiqotlar uchun ishonchli va funksional vosita hisoblanadi.</p>
-                <button className="py-2 px-4 border border-blue-500">Batafsil...</button>
+                <button className="py-2 px-4 border border-blue-500 mb-4">Batafsil...</button>
             </div>
             <div>
                 <img src="https://assets-global.website-files.com/5f8e953a227ca157f67b8a07/6252ce379bdbc967b10cc4ad_E8CcZAlXMAoLLnL.jpg" alt="Virtual Humans" />
