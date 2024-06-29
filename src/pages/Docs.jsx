236b5dc34271d5c353c75cfb0ea1287e3f1723d1
Ernazar <kuanishbayevyernazar@gmail.com>
2024-06-29 18:15:56 +0500

    Docs section content has been changed.

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react'
-import { FaFileDownload } from 'react-icons/fa'
-import { FaAnglesDown } from 'react-icons/fa6'
-
 const Docs = () => {
-    const [expand, setExpand] = useState(false)
   return (
-    <div className='h-min bg-blue-300 md:flex md:items-center md:justify-evenly px-4 py-10 md:px-0'>
-        <div className='relative md:w-[30%] transition ease duration-1000 flex flex-col gap-4'>
-            <img src="./docs/Xulosa_VirtualXimya-1.jpg" alt="Page 1" className='h-full' />
-            {
-                expand && (
-                    <>
-                        <img src="./docs/Xulosa_VirtualXimya-2.jpg" alt="Page 1" className='h-full' />
-                        <img src="./docs/Xulosa_VirtualXimya-3.jpg" alt="Page 1" className='h-full' />
-                    </>
-                )
-            }
-            <div className='bg-white relative bottom-10 h-10 shadow-[0_-10px_10px_#00000029] rounded-[10px_10px_0_0]'>
-                <div className='bg-white px-10 py-1 w-fit mx-auto absolute bottom-full left-1/2 -translate-x-1/2 rounded-[10px_10px_0_0] shadow-[0_-10px_10px_#00000029] cursor-pointer' title='Expand' onClick={() => setExpand(prev => !prev)}>
-                    <FaAnglesDown className={`${expand && 'rotate-180'}`} />
-                </div>
+    <div className="h-screen bg-stone-50">
+        <div className="max-w-[1280px] mx-auto grid grid-cols-2 items-center gap-4">
+            <div>
+                <h1 className="text-2xl font-bold mb-4">Virtual Kimyo Laboratoriyasi</h1>
+                <p className="mb-2 indent-8">"Virtual Kimyo Laboratoriyasi" ilovasi Windows platformasi uchun C# va WinForms texnologiyalaridan foydalangan holda ishlab chiqilgan innovatsion dasturiy ta'minotdir. Ushbu ilova foydalanuvchilarga xavfsiz va interaktiv muhitda kimyoviy tajribalarni amalga oshirish imkoniyatini yaratadi. Dastur kimyo fanini o‘rganayotgan talabalar, o‘qituvchilar va kimyoga qiziqadigan barcha uchun mo‘ljallangan.</p>
+                <p className="mb-2 indent-8">"Virtual Kimyo Laboratoriyasi" ilovasi foydalanuvchilarga kimyoviy tajribalarni o‘tkazish uchun qulay va xavfsiz vositani taqdim etadi. Bu ilova nafaqat nazariyani o'rganish, balki kimyoviy reaktsiyalarning o'tishini amalda ko'rish imkonini beradi. Zamonaviy texnologiyalar va puxta o'ylangan arxitekturadan foydalanish tufayli, ilova ta'lim va ilmiy tadqiqotlar uchun ishonchli va funksional vosita hisoblanadi.</p>
+                <button className="py-2 px-4 border border-blue-500">Batafsil...</button>
+            </div>
+            <div>
+                <img src="https://assets-global.website-files.com/5f8e953a227ca157f67b8a07/6252ce379bdbc967b10cc4ad_E8CcZAlXMAoLLnL.jpg" alt="Virtual Humans" />
             </div>
         </div>
-        <a target='_blank' title='Download file' href="https://docs.google.com/document/d/1YoEOYwhdlsZ4yMxSyYS5RpG2xsiMHC2-/edit?usp=drive_link&ouid=101333697640876358029&rtpof=true&sd=true" className='size-20 md:size-32 rounded-3xl bg-gray-300 flex justify-center items-center mx-auto md:mx-0'>
-            <FaFileDownload fill='black' className='hover:fill-blue-600 size-8' />
-        </a>
     </div>
   )
 }
